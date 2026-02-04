05f0d74fe6a4533c40af953dd663dd1f44d44e53
jstgrowup <deysubham999@gmail.com>
2026-02-04 10:27:26 +0530

    added nodemon

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
 console.log("starting a new project");
+const express = require("express");
+const app = express();
+app.use("/test", (req, res) => {
+  res.send("Hello from server");
+});
+app.listen(8000, () => {
+  console.log("Server started on 3000");
+});
