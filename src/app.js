d4c9b3207d30ff60c169acf729cf1ffa1d26144e
jstgrowup <deysubham999@gmail.com>
2026-02-05 09:37:13 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -4,6 +4,18 @@ const app = express();
 app.use("/test", (req, res) => {
   res.send("Hello from server");
 });
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("first");
+    // res.send("Sent 1");
+    next();
+  },
+  (req, res) => {
+    console.log("second");
+    res.send("Sent 2");
+  },
+);
 app.listen(8000, () => {
-  console.log("Server started on 3000");
+  console.log("Server started on 8000");
 });
